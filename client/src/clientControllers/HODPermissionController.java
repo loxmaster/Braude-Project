b02d55bece25c8055954788d3fa6e7f9c226d033
IPblood <michaeltrifonov21@gmail.com>
2023-05-05 16:55:22 +0300

    screens updated

--- client/src/clientControllers/HODPermissionController.java
+++ client/src/clientControllers/HODPermissionController.java
@@ -1,19 +1,14 @@
 package clientControllers;
 
 
-import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-// idk
-import javafx.fxml.FXMLLoader;
 import javafx.scene.control.TableView;
-import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import javafx.scene.Node;
-import javafx.scene.Scene;
 
 
-public class HODPermissionController {
+public class HODPermissionController extends BasicController{
 
     @FXML
     private TableView<?> table;
@@ -22,30 +17,16 @@ public class HODPermissionController {
     void backPressed(ActionEvent event) {
         // open HOD screen from existing stage
 		Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-		AnchorPane root = null;
-		try {
-			root = FXMLLoader.load(getClass().getResource("/clientFXMLS/HOD.fxml"));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		Scene scene = new Scene(root);
+		openScreen("/clientFXMLS/HOD.fxml", currentStage);
 		currentStage.setTitle("CEMS System - Head Of Department");
-		currentStage.setScene(scene);
     }
 
     @FXML
     void logoutPressed(ActionEvent event) {
         // open Login screen from existing stage
 		Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-		AnchorPane root = null;
-		try {
-			root = FXMLLoader.load(getClass().getResource("/clientFXMLS/LoginScreen.fxml"));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		Scene scene = new Scene(root);
+		openScreen("/clientFXMLS/LoginScreen.fxml", currentStage);
 		currentStage.setTitle("CEMS System - Login");
-		currentStage.setScene(scene);
     }
 
 }
