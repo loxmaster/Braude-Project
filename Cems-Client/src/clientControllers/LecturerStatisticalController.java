4fc4b87f0ce989051e4ea1f204a0e2d005dc8595
kookmao <noahsoskha1@gmail.com>
2023-05-28 23:27:51 +0300

    pulling my hair out

--- Cems-Client/src/clientControllers/LecturerStatisticalController.java
+++ Cems-Client/src/clientControllers/LecturerStatisticalController.java
@@ -7,11 +7,15 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Group;
+import javafx.scene.Scene;
 import javafx.scene.chart.BarChart;
 import javafx.scene.chart.PieChart;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableView;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 public class LecturerStatisticalController extends BasicController {
 
@@ -33,37 +37,51 @@ public class LecturerStatisticalController extends BasicController {
 	// * need to add a Sleep option
 
 	public void initialize_PieChart() {// pass data as argument (?)
-		ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList(
-				new PieChart.Data("Passed", 50),
-				new PieChart.Data("Failed", 30));
 
-		//pieChartData.forEach(
-				//data -> data.nameProperty().bind(Bindings.concat(data.getName(), "amount", data.pieValueProperty())));
+		AnchorPane root = null;
+		Stage currentStage = new Stage();
 
-				
-		System.out.println(pieChart.getData());
+		ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList();
+		pieChartData.add(new PieChart.Data("Passed", 40));
+		pieChartData.add(new PieChart.Data("Failed", 60));
+
+		// this should add little arrows to the piechart:
+		pieChartData.forEach(
+				data -> data.nameProperty().bind(Bindings.concat(data.getName(), "amount",
+						data.pieValueProperty())));
+
+		// assign the data to the pieChart
 		pieChart.getData().addAll(pieChartData);
+
+		Group r = new Group(pieChart);
+		Scene s = new Scene(r, 600, 400);
+
+		currentStage.setTitle("PieChart!!!!!!!!!!!!!!!!!!!!");
+		currentStage.setScene(s);
+		currentStage.show();
+
+		System.out.println(pieChart.getData());
 	}
 
-	public void initialize_BarChart() {// pass data as argument (?)
+	// public void initialize_BarChart() {// pass data as argument (?)
 
-		XYChart.Series series1 = new XYChart.Series<>();
+	// XYChart.Series series1 = new XYChart.Series<>();
 
-		series1.setName("2023");
-		series1.getData().add(new XYChart.Data("tinkywinky", 23532));
-		series1.getData().add(new XYChart.Data("lala", 666));
-		series1.getData().add(new XYChart.Data("po", 11111));
+	// series1.setName("2023");
+	// series1.getData().add(new XYChart.Data("tinkywinky", 23532));
+	// series1.getData().add(new XYChart.Data("lala", 666));
+	// series1.getData().add(new XYChart.Data("po", 11111));
 
-		XYChart.Series series2 = new XYChart.Series<>();
+	// XYChart.Series series2 = new XYChart.Series<>();
 
-		series2.setName("2022");
-		series2.getData().add(new XYChart.Data("abc", 123));
-		series2.getData().add(new XYChart.Data("def", 456));
-		series2.getData().add(new XYChart.Data("ghi", 678));
+	// series2.setName("2022");
+	// series2.getData().add(new XYChart.Data("abc", 123));
+	// series2.getData().add(new XYChart.Data("def", 456));
+	// series2.getData().add(new XYChart.Data("ghi", 678));
 
-		barchart.getData().addAll(series1, series2);
+	// barchart.getData().addAll(series1, series2);
 
-	}
+	// }
 
 	@FXML
 	void backtoLecturerMain(ActionEvent event) {
@@ -78,7 +96,8 @@ public class LecturerStatisticalController extends BasicController {
 	void viewGraph_ButtonPressed(ActionEvent event) {
 		// return to table view from 'Edit Question'
 		// ((Node) event.getSource()).getScene().getWindow().hide();
-		LecturerStatisticalController lsc = (LecturerStatisticalController) openScreen("/clientFXMLS/LecturerStatistical_GraphView.fxml", "CEMS System - Viewing Graph", event);
+		LecturerStatisticalController lsc = (LecturerStatisticalController) openScreen(
+				"/clientFXMLS/LecturerStatistical_GraphView.fxml", "CEMS System - Viewing Graph", event);
 		lsc.initialize_PieChart();
 	}
 
@@ -86,7 +105,8 @@ public class LecturerStatisticalController extends BasicController {
 	void backtoStatistical(ActionEvent event) {
 		// return to table view from 'Edit Question'
 		// ((Node) event.getSource()).getScene().getWindow().hide();
-		openScreen("/clientFXMLS/LecturerStatistical.fxml", "CEMS System - Visualization - Statistical Information",event);
+		openScreen("/clientFXMLS/LecturerStatistical.fxml", "CEMS System - Visualization - Statistical Information",
+				event);
 		// FIXME fix this load table thingy
 		// lecturerStatisticalController.loadTable();
 
