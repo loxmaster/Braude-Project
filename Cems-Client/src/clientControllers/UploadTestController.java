921c0d3a73ed5e7874915e8ac7bf603eb753e546
kookmao <noahsoskha1@gmail.com>
2023-06-16 23:59:44 +0300

    *upload file for lecturer * accept file to student

--- Cems-Client/src/clientControllers/UploadTestController.java
+++ Cems-Client/src/clientControllers/UploadTestController.java
@@ -1,11 +1,22 @@
 package clientControllers;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
+import javax.swing.JOptionPane;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
 public class UploadTestController extends BasicController {
+
+	private String filePath = "none";
 
     @FXML
     private TextField TextPath;
@@ -22,14 +33,50 @@ public class UploadTestController extends BasicController {
     @FXML
     private Button Upload;
 
+	@FXML
+    private TextField testID;
+
+
+
+
     @FXML
     void OpenFileMenu(ActionEvent event) {
+		FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Choose a file");
+        Stage stage = (Stage) chooseFile.getScene().getWindow();
+        File selectedFile = fileChooser.showOpenDialog(stage);
+        if (selectedFile != null) {
+            filePath = selectedFile.getAbsolutePath();
+            TextPath.setText(filePath);
+        }
 
     }
 
     @FXML
     void UploadPressed(ActionEvent event) {
-
+		if (!filePath.isEmpty() && !(filePath=="none") && (!testID.getText().isEmpty())) {
+			try {
+				File selectedFile = new File(filePath);
+				
+				// Create the output stream and object output stream
+				FileOutputStream fileOut = new FileOutputStream("Cems-Server/TestFiles/Testid" + testID.getText() );
+				ObjectOutputStream objectOut = new ObjectOutputStream(fileOut);
+				
+				// Write the selected file object to the output stream
+				objectOut.writeObject(selectedFile);
+				
+				// Close the streams
+				objectOut.close();
+				fileOut.close();
+				
+				System.out.println("File uploaded successfully.");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		} else {
+			JOptionPane.showMessageDialog(null,"No File Chosen", "Error",JOptionPane.INFORMATION_MESSAGE);
+			return;
+		}
     }
 
     @FXML
