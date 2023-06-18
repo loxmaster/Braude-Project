ddfd916e6c6c25af743af819a5db56375b52a385
Danilov2105 <Alexander.Danilov@e.braude.ac.il>
2023-06-18 15:09:47 +0300

    add screne

--- Cems-Client/src/clientControllers/CheckTestController.java
+++ Cems-Client/src/clientControllers/CheckTestController.java
@@ -1,11 +1,31 @@
 package clientControllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableView;
 
 public class CheckTestController extends BasicController {
 
-	@FXML
-	private TableView<?> table;
+    @FXML
+    private Button exitbutton;
+
+    @FXML
+    private Button logo;
+
+    @FXML
+    private TableView<?> table;
+
+    @FXML
+    void backPressed(ActionEvent event) {
+		openScreen("/clientFXMLS/Lecturer1.fxml", "CEMS System - Lecturer", event);
+    }
+
+    @FXML
+    void backToLecturer(ActionEvent event) {
+		openScreen("/clientFXMLS/Lecturer1.fxml", "CEMS System - Lecturer", event);
+    }
+
 
 }
