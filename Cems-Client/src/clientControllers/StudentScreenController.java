5216e0c1fb5bfc725caea686e513ff2ba5b72808
Amir Mishayev <127884315+amir3x0@users.noreply.github.com>
2023-06-18 11:19:51 +0300

    view grades completed

--- Cems-Client/src/clientControllers/StudentScreenController.java
+++ Cems-Client/src/clientControllers/StudentScreenController.java
@@ -10,40 +10,50 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import logic.User;
 
+// Suppressing warnings about unused imports or fields
 @SuppressWarnings("unused")
 
+// This class is a controller for the StudentScreen view
 public class StudentScreenController extends BasicController {
 
-	private User student;
+    // The User object representing the student who is currently logged in
+    private User student;
 
-	@FXML
-	private TextArea helpLabel;
+    // The TextArea that displays help information
+    @FXML
+    private TextArea helpLabel;
 
-	@FXML
-	private Label live_time;
+    // The Label that displays the current time
+    @FXML
+    private Label live_time;
 
-	public void loadStudent(User student) {
-		this.student = student;
-		Timenow(live_time);
-	}
+    // This method is called when the StudentScreen view is loaded. It initializes the student field and starts the clock.
+    public void loadStudent(User student) {
+        // Store the User object representing the logged-in student
+        this.student = student;
 
-	@FXML
-	void LogOutPressed(ActionEvent event) {
-		logoutPressed(event);
-	}
+        // Start the clock
+        Timenow(live_time);
+    }
 
-	@FXML
-	void OpenCodePrompt(ActionEvent event) {
-		// Opening window for code entering
-		openScreen("/clientFXMLS/EnterCodeForTestPreforming.fxml", "CEMS System - Student - Enter Test Code", event);
-	}
+    // This method is called when the Log Out button is pressed. It logs the student out and closes the StudentScreen view.
+    @FXML
+    void LogOutPressed(ActionEvent event) {
+        logoutPressed(event);
+    }
 
-	@FXML
-	void showGrades(ActionEvent event) {
-		// Opening Show Grades screen
-		ViewGradesController vgc = (ViewGradesController) openScreen("/clientFXMLS/ViewGrades.fxml",
-				"CEMS System - Student Grades", event);
-		vgc.initialize();
-	}
+    // This method is called when the Open Code Prompt button is pressed. It opens the EnterCodeForTestPreforming view.
+    @FXML
+    void OpenCodePrompt(ActionEvent event) {
+        // Open the EnterCodeForTestPreforming view
+        openScreen("/clientFXMLS/EnterCodeForTestPreforming.fxml", "CEMS System - Student - Enter Test Code", event);
+    }
 
+    // This method is called when the Show Grades button is pressed. It opens the ViewGrades view.
+    @FXML
+    void showGrades(ActionEvent event) {
+        // Open the ViewGrades view
+        ViewGradesController vgc = (ViewGradesController) openScreen("/clientFXMLS/ViewGrades.fxml",
+                "CEMS System - Student Grades", event);
+    }
 }
--- Cems-Client/src/clientControllers/ViewGradesController.java
+++ Cems-Client/src/clientControllers/ViewGradesController.java
@@ -3,7 +3,6 @@ package clientControllers;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 import javafx.collections.FXCollections;
@@ -20,62 +19,67 @@ import clientHandlers.ClientUI;
 import logic.Test;
 import javafx.event.EventHandler;
 
+// This class is a controller for the ViewGrades view
 public class ViewGradesController extends BasicController {
 
+    // The list of completed tests
     public static ArrayList<Test> completedTestsList;
+
+    // The list of subjects and courses
     private static ArrayList<String> SubjectCourse;
+
+    // A flag variable
     public static boolean flag = false;
+
+    // The value selected in the filterComboBox
     public String value;
 
-    // This object is used for synchronization
-    final Object lock = new Object();
-
+    // The container for the exams
     @FXML
     private GridPane ExamContainer;
 
+    // The text field for the student ID
     @FXML
     private Text Student_ID_Text;
 
+    // The back button
     @FXML
     private Button backButton;
 
+    // The combo box for filtering
     @FXML
     private ComboBox<String> filterComboBox;
 
+    // The exit button
     @FXML
     private Button exitbutton;
 
+    // The filter button
     @FXML
-    private Button filterButton; // Button for user to initiate filter
+    private Button filterButton;
 
+    // This method is called when the back button is pressed. It loads the student
+    // main screen.
     @FXML
     void backButtonPressed(ActionEvent event) {
-        // Loading student main screen
         openScreen("/clientFXMLS/StudentScreen.fxml", "CEMS System - Student", event);
     }
 
+    // This method is called when the ViewGrades view is loaded. It initializes the
+    // view.
     @FXML
     void initialize() {
-
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                ClientUI.chat.getcompletedTestsForStudentList();
-                synchronized (lock) {
-                    lock.notify();
-                }
-            }
-        }).start();
-
-        synchronized (lock) {
-            try {
-                lock.wait();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+        // Get the list of completed tests for the student
+        ClientUI.chat.getcompletedTestsForStudentList();
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
 
+        // Set the student ID text
         Student_ID_Text.setText("Student ID: " + completedTestsList.get(0).getStudentID());
+
         // Populate the ComboBox with course names
         Set<String> courseNames = new HashSet<>();
         for (Test test : completedTestsList) {
@@ -84,14 +88,18 @@ public class ViewGradesController extends BasicController {
                 Thread.sleep(100);
             } catch (InterruptedException e) {
                 e.printStackTrace();
-            } // ... rest of your code ...
-            String courseName = SubjectCourse.get(2); // Assuming Test objects have a getCourseName method
+            }
+            String courseName = SubjectCourse.get(2);
             if (!courseNames.contains(courseName)) {
                 courseNames.add(courseName);
             }
         }
+
+        // Add the "Show all" option to the ComboBox
         courseNames.add("Show all");
         filterComboBox.getItems().addAll(FXCollections.observableArrayList(courseNames));
+
+        // Set the action to be performed when an option is selected in the ComboBox
         filterComboBox.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
@@ -102,37 +110,55 @@ public class ViewGradesController extends BasicController {
     }
 
     public void ExamLoad(String value) {
-        // Clear the ExamContainer
+        // Clear the ExamContainer. This is necessary to ensure that the previous exams
+        // are not shown.
         ExamContainer.getChildren().clear();
 
+        // These variables are used to determine the position of the exam card in the
+        // grid.
         int column = 0;
         int row = 1;
         try {
+            // Loop through each test in the completed tests list.
             for (Test test : completedTestsList) {
+                // Get the course for the test.
                 ClientUI.chat.getCourseForTest(test.getId());
                 try {
+                    // Sleep for a short period to allow the course to be fetched.
                     Thread.sleep(100);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
+                // If the value is "Show all", then all exams should be shown.
                 if (value.equals("Show all")) {
+                    // Create a new FXMLLoader.
                     FXMLLoader fxmlLoader = new FXMLLoader();
+                    // Get the URL for the StudentTestCard.fxml file.
                     URL url = getClass().getResource("/clientFXMLS/StudentTestCard.fxml");
                     if (url == null) {
                         System.out.println("Resource not found. Exiting...");
                         return;
                     }
+                    // Load the StudentTestCard.fxml file.
                     AnchorPane testcard = fxmlLoader.load(url.openStream());
+                    // Get the controller for the test card.
                     TestCardController cardController = fxmlLoader.getController();
+                    // Set the test and subject course for the test card.
                     cardController.setCard(test, SubjectCourse);
+                    // If the column is 4, then reset the column to 0 and increment the row.
                     if (column == 4) {
                         column = 0;
                         ++row;
                     }
+                    // Add the test card to the ExamContainer.
                     ExamContainer.add(testcard, column++, row);
+                    // Set the margin for the test card.
                     GridPane.setMargin(testcard, new Insets(10));
                 } else if (SubjectCourse.get(2).equals(value)) {
-
+                    // If the value is not "Show all", then only the exams for the selected course
+                    // should be shown.
+                    // The code in this block is the same as the code in the "Show all" block, but
+                    // it only executes if the course for the test matches the selected course.
                     FXMLLoader fxmlLoader = new FXMLLoader();
                     URL url = getClass().getResource("/clientFXMLS/StudentTestCard.fxml");
                     if (url == null) {
@@ -149,7 +175,6 @@ public class ViewGradesController extends BasicController {
                     ExamContainer.add(testcard, column++, row);
                     GridPane.setMargin(testcard, new Insets(10));
                 }
-
             }
         } catch (Exception e) {
             System.out.println("An error occurred:");
@@ -157,46 +182,57 @@ public class ViewGradesController extends BasicController {
         }
     }
 
+    // Getter for the list of completed tests for a student
     public static ArrayList<Test> getcompletedTestsForStudentList() {
         return completedTestsList;
     }
 
+    // Setter for the list of completed tests for a student
     public static void setcompletedTestsForStudentList(ArrayList<Test> completedTests) {
         completedTestsList = completedTests;
     }
 
+    // Setter for the list of subjects and courses
     public static void setSubjectsCoursesList(ArrayList<String> SubjectandCourse) {
         SubjectCourse = SubjectandCourse;
     }
 
+    // Getter for the list of completed tests
     public static ArrayList<Test> getCompletedTestsList() {
         return completedTestsList;
     }
 
+    // Setter for the list of completed tests
     public static void setCompletedTestsList(ArrayList<Test> completedTestsList) {
         ViewGradesController.completedTestsList = completedTestsList;
     }
 
+    // Getter for the list of subjects and courses
     public static ArrayList<String> getSubjectCourse() {
         return SubjectCourse;
     }
 
+    // Setter for the list of subjects and courses
     public static void setSubjectCourse(ArrayList<String> subjectCourse) {
         SubjectCourse = subjectCourse;
     }
 
+    // Getter for the flag variable
     public static boolean isFlag() {
         return flag;
     }
 
+    // Setter for the flag variable
     public static void setFlag(boolean flag) {
         ViewGradesController.flag = flag;
     }
 
+    // Getter for the filter combo box
     public ComboBox<String> getFilterComboBox() {
         return filterComboBox;
     }
 
+    // Setter for the filter combo box
     public void setFilterComboBox(ComboBox<String> filterComboBox) {
         this.filterComboBox = filterComboBox;
     }
