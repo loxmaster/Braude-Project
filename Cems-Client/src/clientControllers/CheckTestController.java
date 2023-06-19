b51e59b25a4c93e5f9cd636a17aef8b92b9f3368
Danilov2105 <Alexander.Danilov@e.braude.ac.il>
2023-06-19 14:49:49 +0300

    asda

--- Cems-Client/src/clientControllers/CheckTestController.java
+++ Cems-Client/src/clientControllers/CheckTestController.java
@@ -3,11 +3,16 @@ package clientControllers;
 import java.io.IOException;
 import java.util.ArrayList;
 
+import javax.swing.JOptionPane;
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -16,8 +21,11 @@ import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import javafx.util.Callback;
+import logic.QuestionModel;
 import logic.Test;
 
 public class CheckTestController extends BasicController {
@@ -57,18 +65,44 @@ public class CheckTestController extends BasicController {
 
   @FXML
   void initialize() {
-    Timenow(live_time);
-    //Checked.setCellValueFactory(new PropertyValueFactory<>("checked"));
-    student_id.setCellValueFactory(new PropertyValueFactory<>("Student ID"));
-    Date.setCellValueFactory(new PropertyValueFactory<>("Date"));
-    courseName.setCellValueFactory(new PropertyValueFactory<>("Course"));
-    subjectName.setCellValueFactory(new PropertyValueFactory<>("Subject"));
+    Timenow(live_time); 
+  }
+
+  public void loadTable() {
+
+        //Checked.setCellValueFactory(new PropertyValueFactory<>("checked"));
     test_id.setCellValueFactory(new PropertyValueFactory<>("Test ID"));
-
-    //create a new "manual resolution" button
-    
-
-      
+    subjectName.setCellValueFactory(new PropertyValueFactory<>("Subject"));
+    courseName.setCellValueFactory(new PropertyValueFactory<>("Course"));
+    Date.setCellValueFactory(new PropertyValueFactory<>("Date"));
+    student_id.setCellValueFactory(new PropertyValueFactory<>("Student ID"));
+    Checked.setCellValueFactory(new PropertyValueFactory<>("Checked"));
+  
+    int cap = 20;
+        while (LecturerController.getQuestions().isEmpty() && (cap > 0)) {//??
+            try {
+                Thread.sleep(250);
+                cap--;
+            } catch (InterruptedException e) {
+            }
+        }
+        if (LecturerController.questions.isEmpty()) {//?
+            JOptionPane.showMessageDialog(null, "Error getting the question!", "Error", JOptionPane.ERROR_MESSAGE);
+        } 
+        else {
+            ObservableList<QuestionModel> questionList = FXCollections
+                    .observableArrayList(LecturerController.questions);
+            for (QuestionModel question : questionList){///?
+                question.setEdit(createCheckButton(question));
+                System.err.println(question.getAnswer() + "ANSWER");
+                System.err.println(question.getCoursename() + "COURSE NAME");
+                System.err.println(question.getId() + "ID");
+                System.err.println(question.getLecturer() + "LECTURER");
+                System.err.println(question.getQuestionnumber() + "QUESTION NUMBER");
+                System.err.println(question.getQuestiontext() + "QUESTION TEXT");
+                System.err.println(question.getSubject() + "SUBJECT");
+            }
+        }
   }
 
     //get test data from database
@@ -77,8 +111,8 @@ public class CheckTestController extends BasicController {
 
   
 
-    public Button createEditButton(QuestionModel question) {
-      Button edit = new Button("Edit");
+    public Button createCheckButton(QuestionModel question) {
+      Button edit = new Button("To Check");
 
       edit.setOnAction(new EventHandler<ActionEvent>() {
           @Override
@@ -93,7 +127,7 @@ public class CheckTestController extends BasicController {
               FXMLLoader loader = new FXMLLoader();
               try {
                   final double[] offsets = new double[2];
-                  root = loader.load(getClass().getResource("/clientFXMLS/EditQuestion.fxml").openStream());
+                  root = loader.load(getClass().getResource("/clientFXMLS/EditQuestion.fxml").openStream());//?
                   root.setOnMousePressed(mouseEvent -> {
                       offsets[0] = mouseEvent.getSceneX();
                       offsets[1] = mouseEvent.getSceneY();
@@ -107,18 +141,18 @@ public class CheckTestController extends BasicController {
                   e.printStackTrace();
               }
               EditQuestionController eqc = loader.getController();
-              eqc.loadQuestion(question.getQuestion(), question.getId());
-              System.out.println("opening edit question" + question.getId());
+              eqc.loadQuestion(question.getQuestion(), question.getId());//?
+              System.out.println("opening check tests" + question.getId());
               Scene scene = new Scene(root);
               scene.getStylesheets().add(getClass().getResource("/gui/Stylesheet.css").toExternalForm());
               currentStage.initStyle(StageStyle.UNDECORATED);
               currentStage.setScene(scene);
-              currentStage.setTitle("CEMS System - Lecturer - Edit Question");
+              currentStage.setTitle("CEMS System - Lecturer - Check Test");
               currentStage.show();
           }
       });
 
-      edit.setId("editbutton");
+      edit.setId("checkbutton");
       edit.setPrefWidth(60);
       edit.setPrefHeight(20);
       return edit;
