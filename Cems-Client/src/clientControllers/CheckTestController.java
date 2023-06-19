ff11abab97169bfd864b1ca51f207b4b33ed7b44
Amir Mishayev <127884315+amir3x0@users.noreply.github.com>
2023-06-20 02:09:53 +0300

    checktestCON doc

--- Cems-Client/src/clientControllers/CheckTestController.java
+++ Cems-Client/src/clientControllers/CheckTestController.java
@@ -2,12 +2,9 @@ package clientControllers;
 
 import java.io.IOException;
 import java.util.ArrayList;
-
 import javax.swing.JOptionPane;
-
 import clientHandlers.ClientUI;
 import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.collections.transformation.FilteredList;
@@ -15,25 +12,16 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.RadioButton;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
-import javafx.scene.control.ToggleGroup;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
-import logic.QuestionModel;
 import logic.Test;
 
 public class CheckTestController extends BasicController {
@@ -61,23 +49,30 @@ public class CheckTestController extends BasicController {
   @FXML
   private Label live_time;
 
- 
-
+  /**
+   * This method is called to initialize a controller after its root element has
+   * been completely processed.
+   * It is typically used to perform setup tasks for the controller.
+   */
   @FXML
   void initialize() {
 
     Timenow(live_time);
   }
 
+  /**
+   * This method is used to load data into a table.
+   * It is typically called when initializing the table or when the data source is
+   * updated.
+   */
   public void loadTable() {
 
     // get completed tests list initilized
     ClientUI.chat.getcompletedTestsForLecturerList();
     try {
-        Thread.sleep(250);
-      } catch (InterruptedException e) {
-      }
-    
+      Thread.sleep(250);
+    } catch (InterruptedException e) {
+    }
 
     // Checked.setCellValueFactory(new PropertyValueFactory<>("checked"));
     test_id.setCellValueFactory(new PropertyValueFactory<>("id"));
@@ -101,30 +96,34 @@ public class CheckTestController extends BasicController {
     } else {
       ObservableList<Test> testlist_observable = FXCollections.observableArrayList(completedTestsList);
       for (Test test : completedTestsList) {/// ?
-          test.setCheck(createCheckButton(test));
-        }
-        FilteredList<Test> filteredList = new FilteredList<>(testlist_observable);
-        
-       //  ObservableList<Test> testlist_observable = FXCollections.observableArrayList(completedTestsList);
+        test.setCheck(createCheckButton(test));
+      }
+      FilteredList<Test> filteredList = new FilteredList<>(testlist_observable);
 
+      // ObservableList<Test> testlist_observable =
+      // FXCollections.observableArrayList(completedTestsList);
 
       table.setItems(filteredList);
-      //table.setItems(testlist_observable);
-     // listener - this will update the table to the filtered COMBOBOX SUBJECT
+      // table.setItems(testlist_observable);
+      // listener - this will update the table to the filtered COMBOBOX SUBJECT
       // courseComboBox.getSelectionModel().selectedItemProperty().addListener((observable,
       // oldValue, newValue) -> {
 
-     // updatePredicate(filteredList);
+      // updatePredicate(filteredList);
     }
   }
 
-
-  // MANUAL RESOLUTION BUTTON ACTION
+  /**
+   * This method is used to create a check button for a given test.
+   *
+   * @param test The test for which the check button is created.
+   * @return The created check button.
+   */
   public Button createCheckButton(Test test) {
     Button checked = new Button("Manual Resolution");
-    
+
     checked.setOnAction(new EventHandler<ActionEvent>() {
-      
+
       @Override
       public void handle(ActionEvent event) {
         // remember all the data of the question
@@ -151,9 +150,9 @@ public class CheckTestController extends BasicController {
           e.printStackTrace();
         }
         EvaluateTestController eqc = loader.getController();
-        //TODO get question from the test we sent to this method
+        // TODO get question from the test we sent to this method
         eqc.loadTest(test);
-        //System.out.println("opening edit question" + question.getId());
+        // System.out.println("opening edit question" + question.getId());
         Scene scene = new Scene(root);
         scene.getStylesheets().add(getClass().getResource("/gui/Stylesheet.css").toExternalForm());
         currentStage.initStyle(StageStyle.UNDECORATED);
@@ -168,30 +167,66 @@ public class CheckTestController extends BasicController {
     return checked;
   }
 
+  /**
+   * This method is triggered when the save button is pressed.
+   * It is responsible for handling the save operation.
+   *
+   * @param event The action event that triggers this method.
+   */
   @FXML
   void savePressed(ActionEvent event) {
 
   }
 
+  /**
+   * This method is triggered when the edit comments button is pressed.
+   * It is responsible for handling the operation of editing comments.
+   *
+   * @param event The action event that triggers this method.
+   */
   @FXML
   void editCommentsPressed(ActionEvent event) {
 
   }
 
+  /**
+   * This method is triggered when the back button is pressed.
+   * It is responsible for handling the operation of navigating back from the
+   * current context.
+   *
+   * @param event The action event that triggers this method.
+   */
   @FXML
   void backPressed(ActionEvent event) {
     openScreen("/clientFXMLS/Lecturer1.fxml", "CEMS System - Lecturer", event);
   }
 
+  /**
+   * This method is triggered when the back button is pressed in the context of a
+   * lecturer.
+   * It is responsible for handling the transition back to the lecturer screen.
+   *
+   * @param event The action event that triggers this method.
+   */
   @FXML
   void backToLecturer(ActionEvent event) {
     openScreen("/clientFXMLS/Lecturer1.fxml", "CEMS System - Lecturer", event);
   }
 
+  /**
+   * This method is used to retrieve a list of completed tests.
+   *
+   * @return An ArrayList of completed Test objects.
+   */
   public static ArrayList<Test> getCompletedTestsList() {
     return completedTestsList;
   }
 
+  /**
+   * This method is used to set the list of completed tests.
+   *
+   * @param completedTestsList An ArrayList of completed Test objects to be set.
+   */
   public static void setCompletedTestsList(ArrayList<Test> completedTestsList) {
     CheckTestController.completedTestsList = completedTestsList;
   }
